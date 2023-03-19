f1de94649c930e5bad72c810e0bd23745815673b
Michal Kurzewski <kurzewski.m@gmail.com>
2023-03-19 00:33:39 +0000

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uk.co.codeng',
+  appName: 'capacitor-react',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
