3ad244054d1d31bdccf29b32a687aba805b42b05
Michal Kurzewski <kurzewski.m@gmail.com>
2023-05-05 17:30:38 +0100

    devtools

--- src/components/pages/DevDashboard.tsx
+++ src/components/pages/DevDashboard.tsx
@@ -2,33 +2,25 @@
 import React, { useState } from "react";
 import { useDevDashboard } from "../../contexts/devDashboardContext";
 
-
 const DevDashboard: React.FC = () => {
   const [isDashboardVisible, setDashboardVisible] = useState(false);
   const { logs, toggleVariable } = useDevDashboard();
 
   return (
     <div>
-      <button className="btn fixed bottom-20 right-4 z-30"
+      <button
+        className="btn fixed bottom-20 right-4 z-30 translate-x-full hover:translate-x-0 hover:duration-300"
         onClick={() => setDashboardVisible(!isDashboardVisible)}
       >
         Toggle Dev Dashboard
       </button>
       {isDashboardVisible && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: "rgba(0, 0, 0, 0.8)",
-            zIndex: 20,
-            overflowY: "auto",
-          }}
-        >
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-80 z-20 overflow-y-auto">
           <pre className="text-slate-300">{JSON.stringify(logs, null, 2)}</pre>
-          <button className="text-slate-300" onClick={() => toggleVariable("sampleVariable")}>
+          <button
+            className="text-slate-300"
+            onClick={() => toggleVariable("sampleVariable")}
+          >
             Toggle Sample Variable
           </button>
         </div>
